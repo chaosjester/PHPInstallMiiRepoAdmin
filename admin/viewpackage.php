a15f09b0fd7b9b236cc89e17bee216fa2b710d06
chaosjester <fb@chaosjester.net>
2016-03-21 23:18:31 +1100

    Update
    Word

--- admin/viewpackage.php
+++ admin/viewpackage.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['name']))
   <link rel="stylesheet" type="text/css" href="custom.css">
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Update Packages</title>
+  <title>View Packages</title>
 </head>
 
 <body>

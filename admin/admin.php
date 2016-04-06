c92d95e514ba0b93c36bc652ab081a8cb8635a88
chaosjester <fb@chaosjester.net>
2016-04-06 18:49:09 +1000

    Update
    fixed admin page

--- admin/admin.php
+++ admin/admin.php
@@ -29,13 +29,12 @@ $row = $result->fetch_row();
   <link rel="stylesheet" type="text/css" href="custom.css">
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel='shortcut icon' type='image/x-icon' href='../favicon.ico' />
   <title>Repo Admin</title>
 </head>
 
 <body>
-
-
-<?php include('header.php'); ?>
+  <?php include('header.php'); ?>
    
 <main>
   <br>

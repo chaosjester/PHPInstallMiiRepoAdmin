4a4e3a1ad2d781fdf6883b76e4b361727f6eebbf
chaosjester <fb@chaosjester.net>
2016-06-20 15:52:32 +1000

    Homepage update
    Added mobile menu for admin login

--- custom.js
+++ custom.js
@@ -5,3 +5,10 @@
       // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
       $('.modal-trigger').leanModal();
     });
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
